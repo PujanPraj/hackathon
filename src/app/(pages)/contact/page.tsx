c41718f11928e1d4ea48580b68c49b4b9b5ae46d
Nikhil91191 <nikhilsht91@gmail.com>
2024-09-03 11:51:09 +0545

    done

--- src/app/(pages)/contact/page.tsx
+++ src/app/(pages)/contact/page.tsx
@@ -43,125 +43,134 @@ const page = () => {
   }
   return (
     <>
-      <section className="container flex justify-center items-center gap-6  ">
-        <div className="right w-2/3">
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="flex-col justify-center"
-            >
-              <div className="flex space-x-5 mb-4">
+      <section className="container ">
+        <div className="wrapper flex justify-center items-center gap-6">
+          <div className="right w-2/3">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="flex-col justify-center"
+              >
+                <div className="flex space-x-5 mb-4">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Name:</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter full name..." {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Email:</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter email..." {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="flex space-x-5">
+                  <FormField
+                    control={form.control}
+                    name="phone"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Phone:</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Enter Phone no..."
+                            {...field}
+                            type="tel"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem className="w-1/2">
+                        <FormLabel>Country:</FormLabel>
+                        <FormControl>
+                          <Select>
+                            <SelectTrigger>
+                              <SelectValue placeholder="Nepal" />
+                            </SelectTrigger>
+                            <SelectContent>
+                              <SelectItem value="light">Nepal</SelectItem>
+                              <SelectItem value="dark">India</SelectItem>
+                              <SelectItem value="system">Japan</SelectItem>
+                            </SelectContent>
+                          </Select>
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                </div>
+
                 <FormField
                   control={form.control}
-                  name="username"
+                  name="message"
                   render={({ field }) => (
-                    <FormItem className="w-1/2">
-                      <FormLabel>Name:</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter full name..." {...field} />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem className="w-1/2">
-                      <FormLabel>Email:</FormLabel>
-                      <FormControl>
-                        <Input placeholder="Enter email..." {...field} />
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="flex space-x-5">
-                <FormField
-                  control={form.control}
-                  name="phone"
-                  render={({ field }) => (
-                    <FormItem className="w-1/2">
+                    <FormItem className="w-full h-20">
                       <FormLabel>Phone:</FormLabel>
                       <FormControl>
-                        <Input
-                          placeholder="Enter Phone no..."
+                        <textarea
+                          placeholder="Enter your message..."
                           {...field}
-                          type="tel"
+                          className="w-full p-2 border border-gray-300 rounded-md"
+                          rows={5} // Adjust the number of rows as needed
                         />
                       </FormControl>
                     </FormItem>
                   )}
                 />
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem className="w-1/2">
-                      <FormLabel>Country:</FormLabel>
-                      <FormControl>
-                        <Select>
-                          <SelectTrigger>
-                            <SelectValue placeholder="Nepal" />
-                          </SelectTrigger>
-                          <SelectContent>
-                            <SelectItem value="light">Nepal</SelectItem>
-                            <SelectItem value="dark">India</SelectItem>
-                            <SelectItem value="system">Japan</SelectItem>
-                          </SelectContent>
-                        </Select>
-                      </FormControl>
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <FormField
-                control={form.control}
-                name="message"
-                render={({ field }) => (
-                  <FormItem className="w-full h-20">
-                    <FormLabel>Phone:</FormLabel>
-                    <FormControl>
-                      <textarea
-                        placeholder="Enter your message..."
-                        {...field}
-                        className="w-full p-2 border border-gray-300 rounded-md"
-                        rows={5} // Adjust the number of rows as needed
-                      />
-                    </FormControl>
-                  </FormItem>
-                )}
+              </form>
+            </Form>
+          </div>
+          <div className="left w-1/3 ml-10 mb-10">
+            <div className="phone flex justify-center items-center gap-3 mb-5">
+              <img
+                src="icons/call.png"
+                alt=""
+                className="h-[30px] aspect-square"
               />
-            </form>
-          </Form>
-        </div>
-        <div className="left w-1/3 ml-10 mb-10">
-          <div className="phone flex justify-start items-center gap-3 mb-5">
-            <img
-              src="icons/call.png"
-              alt=""
-              className="h-[30px] aspect-square"
-            />
-            <span>977 980882580</span>
+              <span>977 980882580</span>
+            </div>
+            <div className="phone flex justify-center items-center gap-3 mb-5">
+              <img
+                src="icons/mail.png"
+                alt=""
+                className="h-[30px] aspect-square"
+              />
+              <span>trishul***@gmail.com</span>
+            </div>
+            <div className="phone flex justify-center items-center gap-3">
+              <img
+                src="icons/placeholder.png"
+                alt=""
+                className="h-[30px] aspect-square"
+              />
+              <span>Lokanthali, Bhaktapur</span>
+            </div>
+            <div className="flex justify-start"><iframe src="https://giphy.com/embed/13BlyK63hHAdUc"allowFullScreen className="mt-5 w-full"></iframe></div>
           </div>
-          <div className="phone flex justify-start items-center gap-3 mb-5">
-            <img
-              src="icons/mail.png"
-              alt=""
-              className="h-[30px] aspect-square"
-            />
-            <span>trishul***@gmail.com</span>
-          </div>
-          <div className="phone flex justify-start items-center gap-3">
-            <img
-              src="icons/placeholder.png"
-              alt=""
-              className="h-[30px] aspect-square"
-            />
-            <span>Lokanthali, Bhaktapur</span>
-          </div>
-        </div>
+        </div >
+            <iframe
+              className="w-full mt-32 pb-5"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.331712639985!2d85.36118019999999!3d27.676140799999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb1a1bed1bdd29%3A0x70040fb78c745729!2sSamriddhi%20College!5e0!3m2!1sen!2snp!4v1725338140237!5m2!1sen!2snp"
+              width="600"
+              height="450"
+            ></iframe>
       </section>
     </>
   );
---
+++ src/app/(pages)/signin/page.tsx
@@ -0,0 +1,137 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string().min(8),
+});
+const login = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <>
+      <section className="container">
+        <h1 className="text-[40px] text-center mb-5">Login</h1>
+        <div className="flex justify-center items-center">
+          <div className="wrapper w-2/3 flex gap-5">
+            <div className="left w-1/2 relative">
+              <img
+                src="assets/login/lake-6543167_1280.jpg"
+                alt=""
+                className="h-full opacity-80 rounded-lg"
+              />
+            </div>
+            <div className="right w-1/2">
+              <Form {...form}>
+                <form
+                  onSubmit={form.handleSubmit(onSubmit)}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username:</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Enter username..." {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password:</FormLabel>
+                        <FormControl>
+                          <Input
+                            placeholder="Password here..."
+                            {...field}
+                            type="password"
+                          />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+                  <div className="button-wrapper flex justify-center gap-10">
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                    <Button
+                      type="submit"
+                      className="bg-green-400 text-black w-full"
+                    >
+                      Sigup
+                    </Button>
+                  </div>
+                  <button
+                    type="button"
+                    className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+                  >
+                    <span className="mr-2 inline-block">
+                      <svg
+                        className="h-6 w-6 text-rose-500"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="M20.283 10.356h-8.327v3.451h4.792c-.446 2.193-2.313 3.453-4.792 3.453a5.27 5.27 0 0 1-5.279-5.28 5.27 5.27 0 0 1 5.279-5.279c1.259 0 2.397.447 3.29 1.178l2.6-2.599c-1.584-1.381-3.615-2.233-5.89-2.233a8.908 8.908 0 0 0-8.934 8.934 8.907 8.907 0 0 0 8.934 8.934c4.467 0 8.529-3.249 8.529-8.934 0-.528-.081-1.097-.202-1.625z"></path>
+                      </svg>
+                    </span>
+                    Sign in with Google
+                  </button>
+                  <button
+                    type="button"
+                    className="relative inline-flex w-full items-center justify-center rounded-md border border-gray-400 bg-white px-3.5 py-2.5 font-semibold text-gray-700 transition-all duration-200 hover:bg-gray-100 hover:text-black focus:bg-gray-100 focus:text-black focus:outline-none"
+                  >
+                    <span className="mr-2 inline-block">
+                      <svg
+                        className="h-6 w-6 text-[#2563EB]"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path>
+                      </svg>
+                    </span>
+                    Sign in with Facebook
+                  </button>
+                </form>
+              </Form>
+            </div>
+          </div>
+        </div>
+
+        <p className="text-center mt-5">Don't have account? <Link href={"/signup"} className="text-orange-500">Signup</Link></p>
+      </section>
+    </>
+  );
+};
+
+export default login;
