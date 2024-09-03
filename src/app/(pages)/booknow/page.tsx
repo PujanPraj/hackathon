4445b71cef011f82533e313847a488868800f74c
PujanPraj <puzpraz123@gmail.com>
2024-09-03 15:12:47 +0545

    dsfsdf

--- src/app/(pages)/booknow/page.tsx
+++ src/app/(pages)/booknow/page.tsx
@@ -7,14 +7,14 @@ const Booknow = () => {
     <>
       <PageHeading img="/assets/carousel/carp3.webp" title="Book Now" />
 
-      <section className="container columns-2 space-y-5 py-5 my-5">
-        <div className="flex gap-5 items-center border">
+      <section className="container columns-1 md:columns-2 space-y-5 py-5 my-5">
+        <div className="flex flex-wrap gap-5 items-center border">
           <img
             src="/assets/destination/bhaktapur.jpg"
-            className="w-96 h-64"
+            className="w-full md:w-96 h-64"
             alt="bhaktapur"
           />
-          <div className="space-y-3">
+          <div className="space-y-3 p-4">
             <h1 className="text-3xl font-bold">Bhaktapur</h1>
             <h2 className="text-lg">Price : $1000</h2>
             <Button className="bg-orange-500 hover:bg-orange-400 text-white">
@@ -22,13 +22,13 @@ const Booknow = () => {
             </Button>
           </div>
         </div>
-        <div className="flex gap-5 items-center border">
+        <div className="flex flex-wrap gap-5 items-center border">
           <img
             src="/assets/destination/pokhara.webp"
-            className="w-96 h-64"
+            className="w-full md:w-96 h-64"
             alt="pokhara"
           />
-          <div className="space-y-3">
+          <div className="space-y-3 p-4">
             <h1 className="text-3xl font-bold">Pokhara</h1>
             <h2 className="text-lg">Price : $1000</h2>
             <Button className="bg-orange-500 hover:bg-orange-400 text-white">
@@ -36,13 +36,13 @@ const Booknow = () => {
             </Button>
           </div>
         </div>
-        <div className="flex gap-5 items-center border">
+        <div className="flex flex-wrap gap-5 items-center border">
           <img
             src="/assets/destination/chitwan.jpg"
-            className="w-96 h-64"
+            className="w-full md:w-96 h-64"
             alt="bhaktapur"
           />
-          <div className="space-y-3">
+          <div className="space-y-3 p-4">
             <h1 className="text-3xl font-bold">Chitwan</h1>
             <h2 className="text-lg">Price : $1000</h2>
             <Button className="bg-orange-500 hover:bg-orange-400 text-white">
@@ -50,13 +50,13 @@ const Booknow = () => {
             </Button>
           </div>
         </div>
-        <div className="flex gap-5 items-center border">
+        <div className="flex flex-wrap gap-5 items-center border">
           <img
             src="/assets/destination/lumbini.jpg"
-            className="w-96 h-64"
+            className="w-full md:w-96 h-64"
             alt="lumbini"
           />
-          <div className="space-y-3">
+          <div className="space-y-3 p-4">
             <h1 className="text-3xl font-bold">Lumbini</h1>
             <h2 className="text-lg">Price : $1000</h2>
             <Button className="bg-orange-500 hover:bg-orange-400 text-white">
@@ -64,13 +64,13 @@ const Booknow = () => {
             </Button>
           </div>
         </div>
-        <div className="flex gap-5 items-center border">
+        <div className="flex flex-wrap gap-5 items-center border">
           <img
             src="/assets/destination/kathmandu.webp"
-            className="w-96 h-64"
+            className="w-full md:w-96 h-64"
             alt="kathmandu"
           />
-          <div className="space-y-3">
+          <div className="space-y-3 p-4">
             <h1 className="text-3xl font-bold">Kathmandu</h1>
             <h2 className="text-lg">Price : $1000</h2>
             <Button className="bg-orange-500 hover:bg-orange-400 text-white">
@@ -78,13 +78,13 @@ const Booknow = () => {
             </Button>
           </div>
         </div>
-        <div className="flex gap-5 items-center border">
+        <div className="flex flex-wrap gap-5 items-center border">
           <img
             src="/assets/destination/lalitput.webp"
-            className="w-96 h-64"
+            className="w-full md:w-96 h-64"
             alt="Lalitpur"
           />
-          <div className="space-y-3">
+          <div className="space-y-3 p-4">
             <h1 className="text-3xl font-bold">Lalitpur</h1>
             <h2 className="text-lg">Price : $1000</h2>
             <Button className="bg-orange-500 hover:bg-orange-400 text-white">
